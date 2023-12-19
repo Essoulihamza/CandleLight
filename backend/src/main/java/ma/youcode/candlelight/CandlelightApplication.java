e5205731e994bd6e023f597e7fbea947d8657a5b
Essoulihamza <ehamza.web@gmail.com>
2023-12-19 20:32:52 +0100

    project setup

---
+++ backend/src/main/java/ma/youcode/candlelight/CandlelightApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.candlelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlelightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandlelightApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ma/youcode/candlelight/CandlelightApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.candlelight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandlelightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
