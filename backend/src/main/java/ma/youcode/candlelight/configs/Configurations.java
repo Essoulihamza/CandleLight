b0898a25a45ab7f31ae19568318c3fe15c28e7b9
Essoulihamza <ehamza.web@gmail.com>
2023-12-27 09:32:27 +0100

    sequence service

--- backend/src/main/java/ma/youcode/candlelight/configs/Configurations.java
+++ backend/src/main/java/ma/youcode/candlelight/configs/Configurations.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 @Configuration
 public class Configurations {
     
