325ac3be4d14d825c1971b04818641217078cc25
Essoulihamza <ehamza.web@gmail.com>
2023-12-29 11:59:57 +0100

    add search

--- backend/src/main/java/ma/youcode/candlelight/models/dto/user/UserLoginDto.java
+++ backend/src/main/java/ma/youcode/candlelight/models/dto/user/UserLoginDto.java
@@ -3,12 +3,14 @@ package ma.youcode.candlelight.models.dto.user;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 
